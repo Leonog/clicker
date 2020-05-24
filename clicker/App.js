fc77872d97b4aac8e12219fa24c521aae91cc95e
Leonardo Nogueira <leonardonogu037@gmail.com>
2020-05-23 23:12:10 -0300

    clicker

--- clicker/App.js
+++ clicker/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import AutoClick from './src/autoClick';
-import Dps from './src/dps';
 
-class App extends Component{
+class App extends Component {
 
   state = {
-    autoClick: true,
     rebirthUpgrade: 0,
     ouro: 0,
     dano: 1,
@@ -14,67 +12,74 @@ class App extends Component{
     valUpgrade: 25,
     upgradeLevel: 1,
     defaultHp: 5,
-    hpInimigo: 10,
+    hpInimigo: 5,
     level: 1,
     qtdInimigos: 0,
     enableNextLvl: false,
     showNextLvl: true,
-    dps: 0,
+    dps: [],
     autoDano: 0,
-    defaultAutoUpgrade: 25,
+    defaultAutoUpgrade: 10,
     valAutoUpgrade: 10,
-    autoUpgradeLevel: 1,
+    autoUpgradeLevel: 0,
+    timer: null,
+    danoDps: 0,
+    showUpgrades: false,
   }
 
-  componentDidUpdate(){
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+    this.setState({ timer: null })
+  }
+
+  componentDidMount() {
+    let timer = setInterval(this.calcDps, 1000);
+    this.setState({ timer });
+  }
+
+  calcDps = () => {
+    const { dps } = this.state
+    let dano = 0
+    dps.map((d, i) => {
+      dano = dano + d
+    })
+    this.setState({ danoDps: dano, dps: [] })
+  }
+
+  componentDidUpdate() {
     const { hpInimigo, qtdInimigos, enableNextLvl, level, showNextLvl } = this.state
-    if(hpInimigo <= 0){
+    if (hpInimigo <= 0) {
       this.inimigoMorreu()
     }
-    if(qtdInimigos >= 10 && !enableNextLvl){
-      this.setState({ enableNextLvl: true})
+    if (qtdInimigos >= 10 && !enableNextLvl) {
+      this.setState({ enableNextLvl: true })
     }
-    if(level === 100 && showNextLvl){
-      this.setState({ showNextLvl: false})
+    if (level === 100 && showNextLvl) {
+      this.setState({ showNextLvl: false })
     }
   }
-
-  // useEffect(() => {
-  //   let interval = null
-  //   // if(autoClick){
-  //   //   interval = setInterval(() => {
-  //   //     console.log('auto', autoDano)
-  //   //     hit(autoDano)
-  //   //   }, 1000);
-  //   //   return () => clearInterval(interval);
-  //   // }else{
-  //   //   if(interval){
-  //   //     return () => clearInterval(interval);
-  //   //   }
-  //   // }
-  // }, []);
 
   upgrade = () => {
     const { ouro, valUpgrade, upgradeLevel, defaultUpgrade, dano } = this.state
-    if(ouro >= valUpgrade){
-      this.setState({ 
-        ouro: ouro - valUpgrade ,
+    if (ouro >= valUpgrade) {
+      this.setState({
+        ouro: ouro - valUpgrade,
         upgradeLevel: upgradeLevel + 1,
-        defaultUpgrade: Number.isInteger(upgradeLevel/10) ? defaultUpgrade * 2 : defaultUpgrade,
-        valUpgrade: valUpgrade+1+Number((defaultUpgrade/10).toFixed(0)),
-        dano: dano+1,
+        defaultUpgrade: Number.isInteger(upgradeLevel / 10) ? defaultUpgrade * 2 : defaultUpgrade,
+        valUpgrade: valUpgrade + 1 + Number((defaultUpgrade / 10).toFixed(0)),
+        dano: dano + 1,
       })
     }
   }
 
   autoUpgrade = () => {
-    const { ouro, valAutoUpgrade, autoUpgradeLevel, defaultAutoUpgrade, autoDano} = this.state
-    if(ouro >= valAutoUpgrade){
+    const { ouro, valAutoUpgrade, autoUpgradeLevel, defaultAutoUpgrade, autoDano } = this.state
+    if (ouro >= valAutoUpgrade) {
       this.setState({
         ouro: ouro - valAutoUpgrade,
         autoUpgradeLevel: autoUpgradeLevel + 1,
-        defaultAutoUpgrade: Number.isInteger(autoUpgradeLevel/10) ? defaultAutoUpgrade*2 : defaultAutoUpgrade,
-        valAutoUpgrade: valAutoUpgrade+1+Number((defaultAutoUpgrade/10).toFixed(0)),
+        defaultAutoUpgrade: Number.isInteger(autoUpgradeLevel / 10) ? defaultAutoUpgrade * 2 : defaultAutoUpgrade,
+        valAutoUpgrade: valAutoUpgrade + 1 + Number((defaultAutoUpgrade / 10).toFixed(0)),
         autoDano: autoDano + 1
       })
     }
@@ -83,24 +88,26 @@ class App extends Component{
   recebeOuro = () => {
     const { level } = this.state
     let rand = Math.random().toFixed(2) * 2
-    if(rand < 1) rand = 1
+    if (rand < 1) rand = 1
     return (rand * level).toFixed(0)
   }
 
   inimigoMorreu = () => {
     const { qtdInimigos, defaultHp, ouro } = this.state
     this.setState({
-      qtdInimigos: qtdInimigos+1,
+      qtdInimigos: qtdInimigos + 1,
       hpInimigo: defaultHp,
       ouro: ouro + Number(this.recebeOuro())
     })
   }
 
   hit = (damage) => {
-    const { hpInimigo, rebirthUpgrade, dps} = this.state
+    const { hpInimigo, rebirthUpgrade, dps } = this.state
+    let dano = damage + (damage * rebirthUpgrade)
+    dps.push(dano)
     this.setState({
-      hpInimigo: hpInimigo - (damage + (damage * rebirthUpgrade)),
-      dps: dps + (damage + (damage * rebirthUpgrade)),
+      hpInimigo: hpInimigo - dano,
+      dps,
     })
   }
 
@@ -124,9 +131,9 @@ class App extends Component{
   nextLevel = () => {
     const { defaultHp, level } = this.state
     let newHp
-    if(Number.isInteger(level/10)){
-      newHp = (defaultHp*2) + 5
-    }else{
+    if (Number.isInteger(level / 10)) {
+      newHp = (defaultHp * 2) + 5
+    } else {
       newHp = defaultHp + 5
     }
     this.setState({
@@ -138,26 +145,83 @@ class App extends Component{
     })
   }
 
-  render(){
+  render() {
     const styles = StyleSheet.create({
       container: {
+        height: '100%',
+        backgroundColor: '#f1f1f1',
+      },
+      content: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 20,
+        height: '100%',
+      },
+      headRow: {
+        display: 'flex',
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      headRowCol: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+      },
+      bottomMenu: {
+        position: "absolute",
+        bottom: 0,
+        width: '100%',
+        display: "flex",
+        flexDirection: 'column',
+      },
+      bottomMenuRow: {
+        width: '100%',
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+      },
+      bottomMenuContent: {
+        width: '100%',
+        backgroundColor: '#e5e5e5',
+        borderStyle: 'solid',
+        borderColor: '#e5e5e5',
+        borderTopLeftRadius: 8,
         padding: 20,
       },
-      button: {
-    
+      upgradesButton: {
+        borderStyle: "solid",
+        borderColor: "#4a5b64",
+        backgroundColor: "#4a5b64",
+        borderWidth: 1,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        elevation: 5,
       },
-    
+      upgradesButtonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 18,
+      },
+      upgradesRow: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: '100%',
+        padding: 5,
+      }
     });
-    const { 
-      ouro, 
-      upgradeLevel, 
-      dano, 
-      rebirthUpgrade, 
-      autoUpgradeLevel, 
-      autoDano, 
+    const {
+      ouro,
+      upgradeLevel,
+      dano,
+      rebirthUpgrade,
+      autoUpgradeLevel,
+      autoDano,
       hpInimigo,
       qtdInimigos,
       level,
@@ -165,97 +229,204 @@ class App extends Component{
       valAutoUpgrade,
       enableNextLvl,
       showNextLvl,
-      autoClick,
+      danoDps,
+      defaultHp,
     } = this.state
     return (
       <View style={styles.container}>
-        <Text>Ouro</Text>
-        <Text>{ouro}</Text>
-        <Text>Upgrade Dano</Text>
-        <Text>{upgradeLevel}</Text>
-        <Text>Dano</Text>
-        <Text>{dano + (dano*rebirthUpgrade)}</Text>
-        <Text>Upgrade autoClick</Text>
-        <Text>{autoUpgradeLevel}</Text>
-        <Text>AutoDamage</Text>
-        <Text>{autoDano + (autoDano*rebirthUpgrade)}</Text>
-        <Text>HP</Text>
-        <Text>
-          {hpInimigo < 0 ? '0' : (
-            (hpInimigo > 100) ?
-              hpInimigo.toFixed(0)
-            :
-              hpInimigo.toFixed(2)
-          )}
-        </Text>
-        <Text>qtdInimigos</Text>
-        <Text>{qtdInimigos}</Text>
-        <Text>Level</Text>
-        <Text>{level}</Text>
-        <TouchableOpacity
-          style={{
+        <View style={styles.content}>
+          <View style={styles.headRow}>
+            <View style={styles.headRowCol}>
+              <Text style={{
+                fontWeight: 'bold',
+              }}>DPS</Text>
+              <Text style={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                color: danoDps > (dano + autoDano) * 6 ? 'red' : (danoDps > (dano + autoDano) * 3 ? 'orange' : '#000')
+              }}>
+                {danoDps === 0 ? 0 : (
+                  danoDps < 10 ?
+                    danoDps.toFixed(2)
+                    :
+                    danoDps.toFixed(0)
+                )}
+              </Text>
+            </View>
+            <View style={styles.headRowCol}>
+              <Text style={{
+                fontWeight: 'bold',
+              }}>Level</Text>
+              <Text style={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                color: '#000'
+              }}>{level}</Text>
+            </View>
+            <View style={styles.headRowCol}>
+              <Text style={{
+                fontWeight: 'bold',
+              }}>Ouro</Text>
+              <Text style={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                color: '#000'
+              }}>{ouro}</Text>
+            </View>
+          </View>
+
+          <View style={{
             display: 'flex',
-            backgroundColor: 'red',
-            padding: 40,
-          }}
-          onPress={() => {
-            this.hit(dano)
-          }} 
-        >
-          <Text style={{
-            fontSize: 24,
-            color: '#FFF',
-            fontWeight: 'bold',
-          }}>CLIQUE</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            backgroundColor: ouro < valUpgrade ? '#c5c5c5' : 'blue', 
-            padding: 20}}
-          onPress={() => {
-            this.upgrade()
-          }} 
-        >
-          <Text style={{color: '#FFF'}}>Comprar Upgrade ${valUpgrade}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            backgroundColor: ouro < valAutoUpgrade ? '#c5c5c5' : 'orange', 
-            padding: 20}}
-          onPress={() => {
-            this.autoUpgrade()
-          }} 
-        >
-          <Text style={{color: '#FFF'}}>Comprar Upgrade autoClick ${valAutoUpgrade}</Text>
-        </TouchableOpacity>
-        {enableNextLvl && showNextLvl &&
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start',
+            width: '70%',
+            backgroundColor: '#a3a3c2',
+            padding: 3,
+            borderRadius: 3,
+            height: 30,
+            position: 'relative',
+            elevation: 5,
+            marginTop: 20,
+          }}>
+            <View style={{
+              width: `${(hpInimigo / defaultHp) * 100}%`,
+              height: '100%',
+              backgroundColor: '#c00',
+              position: 'absolute',
+              marginTop: 3,
+              marginLeft: 3,
+              borderRadius: 2,
+            }} />
+            <View style={{ position: 'absolute', display: 'flex', alignSelf: 'center', marginTop: 4 }}>
+              <Text style={{ color: '#FFF', fontWeight: 'bold' }}>
+                {hpInimigo < 0 ? '0' : (
+                  (hpInimigo > 100) ?
+                    hpInimigo.toFixed(0)
+                    :
+                    hpInimigo.toFixed(2)
+                )}/{defaultHp}
+              </Text>
+            </View>
+          </View>
+          <View style={{
+            padding: 15,
+          }}>
+            <Text style={{
+              fontWeight: 'bold',
+              fontSize: 16,
+            }}>{qtdInimigos > 9 ? '' : `${qtdInimigos}/10`}</Text>
+          </View>
+
           <TouchableOpacity
-            style={{backgroundColor: 'green', padding: 20}}
+            style={{
+              display: 'flex',
+              backgroundColor: 'red',
+              padding: 40,
+            }}
             onPress={() => {
-              this.nextLevel()
+              this.hit(dano)
             }}
           >
-            <Text>
-              Next LVL
-            </Text>
+            <Text style={{
+              fontSize: 24,
+              color: '#FFF',
+              fontWeight: 'bold',
+            }}>CLIQUE</Text>
           </TouchableOpacity>
-        }
-        {level === 100 &&
-          <TouchableOpacity
-            style={{backgroundColor: 'black', padding: 20}}
-            onPress={() => {
-              this.rebirth()
-            }}
-          >
-            <Text style={{color: '#FFF'}}>
-              REBIRTH
-            </Text>
-          </TouchableOpacity>
-        }
-        <AutoClick 
-          hit={this.hit}
-          dano={autoDano}
-        />
+          {level === 100 &&
+            <TouchableOpacity
+              style={{ backgroundColor: 'black', padding: 20 }}
+              onPress={() => {
+                this.rebirth()
+              }}
+            >
+              <Text style={{ color: '#FFF' }}>
+                REBIRTH
+              </Text>
+            </TouchableOpacity>
+          }
+        </View>
+        <View style={styles.bottomMenu}>
+          {enableNextLvl && showNextLvl &&
+            <View style={styles.bottomMenuRow}>
+              <TouchableOpacity style={{
+                backgroundColor: '#ffbf00',
+                padding: 15,
+                paddingLeft: 30,
+                paddingRight: 30,
+                borderRadius: 50,
+                elevation: 8,
+                marginRight: 20,
+                marginBottom: 40,
+              }}
+                onPress={() => {
+                  this.nextLevel()
+                }}
+              >
+                <Text style={{
+                  fontSize: 20,
+                  color: '#FFF',
+                  fontWeight: 'bold'
+                }}>Next Level</Text>
+              </TouchableOpacity>
+            </View>
+          }
+          <View style={styles.bottomMenuRow}>
+            <TouchableOpacity
+              style={styles.upgradesButton}
+              onPress={() => {
+                this.setState({ showUpgrades: !this.state.showUpgrades })
+              }}
+            >
+              <Text style={styles.upgradesButtonText}>Upgrades {!this.state.showUpgrades ? '⇑' : '⇓'}</Text>
+            </TouchableOpacity>
+          </View>
+          {this.state.showUpgrades &&
+            <View style={styles.bottomMenuContent}>
+              <View style={styles.upgradesRow}>
+                <Text>
+                  AutoClick
+                </Text>
+                <AutoClick
+                  hit={this.hit}
+                  dano={autoDano}
+                />
+              </View>
+              <View style={styles.upgradesRow}>
+                <Text>
+                  Dano Clique: {dano}
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: ouro < valUpgrade ? '#c5c5c5' : 'green',
+                    padding: 4
+                  }}
+                  onPress={() => {
+                    this.upgrade()
+                  }}
+                >
+                  <Text style={{ color: '#FFF', fontWeight: "bold" }}>Comprar ${valUpgrade}</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.upgradesRow}>
+                <Text>
+                  Dano Idle: {autoDano}/s
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: ouro < valAutoUpgrade ? '#c5c5c5' : 'green',
+                    padding: 4
+                  }}
+                  onPress={() => {
+                    this.autoUpgrade()
+                  }}
+                >
+                  <Text style={{ color: '#FFF', fontWeight: "bold" }}>Comprar ${valAutoUpgrade}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          }
+        </View>
       </View>
     );
   }
